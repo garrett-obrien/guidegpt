9c11ce185ba69ebe3e36ac49ffca5538c717f8a0
garrett-obrien <g.thomasobrien@gmail.com>
2023-04-09 10:22:12 -0700

    Add starting URLs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # fixie-guidegpt
 A tool for exploring mountain trip reports using LLMs, deployed using fixieai.
 
-Consults local PNW forums and general knowledge about backcountry skiing.
+Consults local PNW forums, blogs, and general knowledge about backcountry skiing.
--- main.py
+++ main.py
@@ -3,25 +3,34 @@ import fixieai
 BASE_PROMPT = """
 I am an agent that helps users research backcountry ski trips in the Pacific Northwest US.
 
-I have general knowledge and the ability to search local forums for more information about
-specific ski objectives and regions. A user may ask me follow up questions, but I always
+I have general knowledge from my training and the ability to search local forums for more information 
+about specific ski objectives and regions. A user may ask me follow up questions, but I always
 do Ask Func[fixie_query_corpus] with a complete question, without any
 reference.
+
+When I provide answers, I share a general overview of possible ski touring routes near the area,
+as well as URLs where a user can access additional information.
 """
 
 FEW_SHOTS = """
-Q: Sample query to this agent
-A: Sample response
+Q: What do you know about touring Ruby Mountain?
+A: Ruby Mountain is a popular ski tour that begins near the gate of Highway 20 in the North
+Cascades National Park.
 
 Q: Another sample query
-Ask Func[example]: input
-Func[example] says: output
+Ask Func[fixie_query_corpus]: input
+Func[fixie_query_corpus] says: output
 A: The other response is output
 """
-agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
 
+# List of PNW forums/blogs/etc. to use in the doc corpus
+URLS = [
+    "https://turns-all-year.com/trip-reports/*",
+    "https://climberkyle.com/category/trip-reports/*",
+    "https://www.whereiskylemiller.com/trip-report/trip-reports/*",
+]
+# Build the document corpus
+DOCS = [fixieai.DocumentCorpus(urls=URLS)]
 
-@agent.register_func
-def example(query: fixieai.Message) -> str:
-    assert query.text == "input"
-    return "output"
+# Assemble the agent
+agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS, DOCS)
