ba379c94119035e2760738947187cf4b480f084e
garrett-obrien <g.thomasobrien@gmail.com>
2023-04-11 16:45:25 -0700

    revert to text-davinci-003

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ Cascades region. You can read more about Ruby Mountain ski touring here: https:/
 Q: Where should I go touring near Snoqualmie Mountain?
 Thought: I need to get search results for ski touring trip reports on Snoqualmie Mountain, Washington.
 Ask Func[fixie_query_corpus]: What ski touring objectives feature Snoqualmie Mountain?
-Func[fixie_query_corpus] says: Popular ski touring objectives on Snoqualmie Mountain include the Slot Couloir, Snot Couloir, and Crooked Couloir.
-A: Popular ski touring objectives on Snoqualmie Mountain include the Slot Couloir, Snot Couloir, and Crooked Couloir. You can read more about Snoqualmie Mountain ski touring here: https://engineeredforadventure.com/slot-snot-couloirs/
+Func[fixie_query_corpus] says: Popular ski touring objectives on Snoqualmie Mountain include the Slot Couloir, Snot Couloir, 
+and Crooked Couloir.
+A: Popular ski touring objectives on Snoqualmie Mountain include the Slot Couloir, Snot Couloir, and Crooked Couloir. 
+You can read more about Snoqualmie Mountain ski touring here: https://engineeredforadventure.com/slot-snot-couloirs/
 Q: Which one is most popular?
 Thought: I need to provide the most frequently mentioned route between the Slot Couloir, Snot Couloir, and Crooked Couloir.
 Ask Func[fixie_query_corpus]: What is most frequently mentioned: the Slout Couloir, Snot Couloir, or Crooked Couloir?
@@ -37,7 +39,9 @@ URLS = [
     "https://turns-all-year.com/trip-reports/ruby-mountain-2", 
     "https://turns-all-year.com/trip-reports/ruby-mountain", 
     "https://turns-all-year.com/trip-reports/march-31-ruby-mountain-north-cascades", 
-    "https://climberkyle.com/*", 
+    "https://climberkyle.com/2021/*",
+    "https://climberkyle.com/2022/*", 
+    "https://climberkyle.com/2023/*" 
     "https://engineeredforadventure.com/*"
     ""
 ]
@@ -53,5 +57,5 @@ agent = fixieai.CodeShotAgent(
     CORPORA,
     conversational=True,
     llm_settings=fixieai.LlmSettings(
-        temperature=0, model="openai/gpt-3.5-turbo", maximum_tokens=2000)
+        temperature=0, model="openai/text-davinci-003", maximum_tokens=2000)
 )
